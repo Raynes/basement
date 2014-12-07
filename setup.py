b6861498ee910a9183981ff4d5351e6852b9ad3b
Anthony Grimes <i@raynes.me>
2014-12-07 15:07:43 -0800

    inc version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md') as f:
 setup(
     name='basement',
     description="A python project scaffolding generator.",
-    version='0.2.1',
+    version='0.2.2',
     long_description=description,
     packages=['basement'],
     include_package_data=True,
